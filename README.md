f5ebc5b6705f6d944a1b172705499b337c09ea6e
Devil-codes <devilgames1999@gmail.com>
2025-08-06 22:39:26 +0530

    added api implementation

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-This is Readme file
+# Project Overview
+
+This is a simple task manager service.
+
+# Setup Instruction
+
+git clone https://github.com/airtribe-projects/task-manager-api-godofgenai.git
+npm i
+npm run start
+
+# Endpoint Details
+
+GET /tasks: Retrieve all tasks.
+GET /tasks/:id: Retrieve a specific task by its ID.
+POST /tasks: Create a new task with the required fields (title, description, completed).
+PUT /tasks/:id: Update an existing task by its ID.
+DELETE /tasks/:id: Delete a task by its ID.
--- app.js
+++ app.js
@@ -5,13 +5,95 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const task = require('./task.json');
+
+let lastId = task['tasks'].length;
+
+const validatePayload = (payload) => {
+    const haveAllProps = payload.title && payload.description && typeof payload.completed == 'boolean';
+    const typeMatched = typeof payload.title == 'string' && typeof payload.description == 'string';
+    return haveAllProps && typeMatched;
+};
+
+app.get('/healthCheck', (req, res) => {
+    res.send('Service is in good health!');
+});
+
+app.get('/tasks', (req, res) => {
+    res.json(task['tasks']);
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    if (!id) res.status(404).json({ message: 'Non existant id' });
+    const tasks = task['tasks'];
+    for (const task of tasks) {
+        if (task.id == id) {
+            return res.json(task);
+        }
+    }
+    res.status(404).json({ message: 'Non existant id' });
+});
+
+app.post('/tasks', (req, res) => {
+    const payloadIsValid = validatePayload(req.body);
+    if (!payloadIsValid) {
+        return res.status(400).json({ message: 'Invalid input' });
+    }
+    lastId += 1;
+    task['tasks'].push({ id: lastId, ...req.body });
+    res.json(task['tasks']);
+});
+
+app.put('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    if (!id) res.status(404).json({ message: 'Non existant id' });
+    const payloadIsValid = validatePayload(req.body);
+    console.log('payload is valid', payloadIsValid);
+    if (!payloadIsValid) {
+        res.status(400).json({ message: 'Invalid input' });
+    }
+    let idFound = false;
+    task['tasks'] = task['tasks'].map((task) => {
+        console.log('iterating', task.id);
+        if (task.id == id) {
+            idFound = true;
+            console.log('id is found');
+            return {
+                id: task.id,
+                ...req.body
+            };
+        }
+        return task;
+    });
+    if (!idFound) {
+        res.status(404).json({ message: 'Non existant id' });
+    }
+    res.json(task['tasks']);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    if (!id) res.status(404).json({ message: 'Non existant id' });
+    let idFound = false;
+    let deletedData;
+    task['tasks'] = task['tasks'].filter((task) => {
+        if (task.id == id) {
+            idFound = true;
+            deletedData = task;
+            return false;
+        }
+        return true;
+    });
+    if (!idFound) res.status(404).json({ message: 'Non existant id' });
+    res.json(deletedData);
+});
+
 app.listen(port, (err) => {
     if (err) {
         return console.log('Something bad happened', err);
     }
     console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
